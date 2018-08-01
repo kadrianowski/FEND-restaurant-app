a75acf43afe056e563b20e8cd22163c0c067a799
Kamil Adrianowski <kamil@e-strona.net>
2018-08-01 08:19:26 +0200

    a11y improvements, headings in correct order

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   image.setAttribute('alt',`image of ${restaurant.name}`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h3');
   name.innerHTML = restaurant.name;
   li.append(name);
 
